d42cd796c300cd453f0b54341eabde0329ec7c00
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-06-01 08:34:17 -0300

    Configuração da recuperação da senha - MAIL

--- e-services/app/Controllers/Http/ForgotPasswordController.js
+++ e-services/app/Controllers/Http/ForgotPasswordController.js
@@ -1,22 +1,41 @@
 'use strict'
 
 const crypto = require('crypto')
+const Mail = use('Mail')
 const User = use('App/Models/User')
 
 class ForgotPasswordController {
-  async store ({ request, response }) {
+  async store({ request, response }) {
     try {
       const email = request.input('email')
       const user = await User.findByOrFail('email', email)
 
+      // SALVANDO NO BANCO
       user.token = crypto.randomBytes(10).toString('hex')
       user.token_created_at = new Date()
       await user.save()
+
+      // ENVIANDO POR EMIAL
+      await Mail.send('emails.welcome', {}, message => {
+        message
+          .from('<no-reply-ti@centenariodosul.pr.gov.br>')
+          .to(user.email)
+          .subject(
+            'Recuperação de senha | Sistema e-maisCidadão - Governo Municipal'
+          )
+      })
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Email não cadastrado.' }
       })
     }
+  }
+
+  async update({ request }) {
+    const password = request.input('password')
+    const token = request.query
+
+    console.log(token)
   }
 }
 
