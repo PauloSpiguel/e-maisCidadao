c81f8d4f1568bfd8ef5dbaf9301da3987f478773
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-06-04 07:06:10 -0300

    Configuração de recuperação de senha e envio de email de confirmação

--- e-services/app/Controllers/Http/ForgotPasswordController.js
+++ e-services/app/Controllers/Http/ForgotPasswordController.js
@@ -1,12 +1,15 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
+const requestIp = require('request-ip')
 const Mail = use('Mail')
 const User = use('App/Models/User')
 
 class ForgotPasswordController {
   async store({ request, response }) {
     try {
+      moment.locale('pt-br')
       const email = request.input('email')
       const user = await User.findByOrFail('email', email)
 
@@ -21,7 +24,8 @@ class ForgotPasswordController {
         {
           email,
           token: user.token,
-          link: `${request.input('redirect_url')}?token=${user.token}`
+          link: `${request.input('redirect_url')}?token=${user.token}`,
+          dateNow: moment().format('LLLL')
         },
         message => {
           message
@@ -40,11 +44,56 @@ class ForgotPasswordController {
     }
   }
 
-  async update({ request }) {
-    const password = request.input('password')
-    const token = request.query
+  async update({ request, response }) {
+    try {
+      const clientIp = requestIp.getClientIp(request)
+      const { password, token } = request.all()
 
-    console.log(token)
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: {
+            message: 'Token expirado. Tente recuperar a senha novamente'
+          }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+      await user.save()
+
+      // ENVIANDO POR EMIAL
+      await Mail.send(
+        ['emails.forgot_success'],
+        {
+          username: user.username,
+          email: user.email,
+          password,
+          link: `http://www.meusite.com/forgot_password`,
+          dateNow: moment().format('LLLL'),
+          clientIp
+        },
+        message => {
+          message
+            .to(user.email)
+            .from(
+              'no-reply-ti@centenariodosul.pr.gov.br',
+              'Sistema e-maisCidadão - Governo Municipal'
+            )
+            .subject('Recuperação de senha - Sucesso')
+        }
+      )
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Algo de errado ao recuperar a senha.' }
+      })
+    }
   }
 }
 
