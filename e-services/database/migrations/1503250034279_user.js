6a4bd9e5edc412ac51c155e1227beb3f484f7aa0
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-06-06 23:14:05 -0300

    Upload de arquivos e view de arquivos

--- e-services/database/migrations/1503250034279_user.js
+++ e-services/database/migrations/1503250034279_user.js
@@ -3,7 +3,7 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
+  up() {
     this.create('users', table => {
       table.increments()
       table
@@ -21,7 +21,7 @@ class UserSchema extends Schema {
     })
   }
 
-  down () {
+  down() {
     this.drop('users')
   }
 }
--- e-services/start/routes.js
+++ e-services/start/routes.js
@@ -6,3 +6,6 @@ Route.post('/users', 'UserController.store')
 Route.post('/sessions', 'SessionController.store')
 Route.post('/forgot-password', 'ForgotPasswordController.store')
 Route.put('/forgot-password', 'ForgotPasswordController.update')
+
+Route.get('/files/:id', 'fileController.show')
+Route.post('/files', 'fileController.store')
