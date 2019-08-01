6fb1ac71c1e503c35e97a2c61329ce8ebbc20bed
Paulo Spiguel <paulospil@hotmail.com>
2019-08-01 13:17:40 -0300

    REMOVE ADDRESS USER

--- e-services/backend/app/Controllers/Http/UserController.js
+++ e-services/backend/app/Controllers/Http/UserController.js
@@ -1,21 +1,21 @@
 'use strict'
 
 const User = use('App/Models/User')
-const Database = use('Database')
+// const Database = use('Database')
 
 class UserController {
   async store({ request }) {
     const data = request.only(['username', 'email', 'password', 'profile'])
 
-    const addresses = request.input('addresses')
+    // const addresses = request.input('addresses')
 
-    const trx = await Database.beginTransaction()
+    // const trx = await Database.beginTransaction()
 
-    const user = await User.create(data, trx)
+    const user = await User.create(data)
 
-    await user.addresses().createMany(addresses, trx)
+    // await user.addresses().createMany(addresses, trx)
 
-    await trx.commit()
+    // await trx.commit()
 
     return user
   }
